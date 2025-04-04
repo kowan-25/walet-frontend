5a43f804d7f69f58c94a4a5f877bd4b016ec7603
matthewhjt <matthewhjt72@gmail.com>
2025-04-04 09:30:29 +0700

    feat: update financial analysis interface

--- src/app/dashboard/[id]/financial-analysis/interface.ts
+++ src/app/dashboard/[id]/financial-analysis/interface.ts
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface ProjectAnalysisResponse {
     month: number;
     year: number;
@@ -21,17 +23,22 @@ export interface TopMember {
     percentage: number;
 }
 
-export const MONTHS: Record<number, string> = {
-    1: "January",
-    2: "February",
-    3: "March",
-    4: "April",
-    5: "May",
-    6: "June",
-    7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December"
-};
+export interface MonthDialogProps {
+    open: boolean
+    onOpenChange: (open: boolean) => void
+    tempMonth: number
+    setTempMonth: Dispatch<SetStateAction<number>>
+    yearError: string
+    setYearError: Dispatch<SetStateAction<string>>
+    tempYear: number
+    setTempYear: Dispatch<SetStateAction<number>>
+    setMonth: Dispatch<SetStateAction<number>>
+    setYear: Dispatch<SetStateAction<number>>
+    setDialogOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export interface FinancialCardProps {
+    data?: ProjectAnalysisResponse
+    month: number
+    year: number
+}
